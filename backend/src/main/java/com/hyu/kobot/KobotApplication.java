5c71625847d4803d3a558d1f7c21403a05d6a952
brorae <jhr3239@gmail.com>
2022-12-23 16:21:37 +0900

    chore: init project

---
+++ backend/src/main/java/com/hyu/kobot/KobotApplication.java
@@ -0,0 +1,13 @@
+package com.hyu.kobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KobotApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hyu/kobot/KobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyu.kobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
