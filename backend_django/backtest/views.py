c4ef45fe9add386dd815d6655124995923039cc0
Jungyoon Han <jyoon0c11@gmail.com>
2023-02-28 02:17:52 +0900

    app 생성

--- backend_django/backtest/views.py
+++ backend_django/backtest/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	message = request.GET.get('abc')
+	print(message)
+
+	return HttpResponse("backtest")
+
 
 # Create your views here.
