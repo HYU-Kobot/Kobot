d8f310b6a99811fd3f998056eed3521a176aa580
김민석 <devwill409@gmail.com>
2023-05-30 16:25:33 +0900

    Feat/26 final fe (#26)
    * feat: 백테스트 중 로딩창, 로그인 api 완료
    
    * feat: api 수정, 포트폴리오 레이아웃, 봇 추가, 삭제, 조회, 자잘한 수정사항들
    
    ---------
    
    Co-authored-by: Kim minseok <>

--- frontend/src/views/portfolios/index.js
+++ frontend/src/views/portfolios/index.js
@@ -1,4 +1,4 @@
-import {Box, Button, Grid, Link, Modal} from '@mui/material';
+import {Box, Button, Grid, Link, Modal, Typography} from '@mui/material';
 import MuiTypography from '@mui/material/Typography';
 
 // project imports
@@ -7,93 +7,406 @@ import MainCard from 'ui-component/cards/MainCard';
 import { gridSpacing } from 'store/constant';
 import * as React from "react";
 import AddBotModal from "./AddBotModal";
+import BotTemplate from "./BotTemplate";
+import OrderHistoryComponent from "../back-test/OrderHistoryComponent";
+import {useContext, useEffect} from "react";
+import axios from "axios";
+import BackTestContext from "../back-test/BackTestContext";
+import Context from "../../Context";
 
 // ==============================|| TYPOGRAPHY ||============================== //
 
-let totalNetCopyAmount = 320
-let totalPortfolioMarginBalance = 3242
-let totalRealizedPNL = 3242
-let netProfit = 23423
-let botname = 'Bot 1'
 
 const Index = () => {
 
-    const [botList, setBotList] = React.useState([
-        {
-            'name' : 'Bot 1',
-            'strategy' : 'A',
-            'inputMoney' : 1231245,
-            'coin' : 'Bitcoin',
+    const [balance, setBalance] = React.useState({
+        "moneyOnHand": 10000,
+        "purchaseAmountSum": 20000,
+        "netProfitSum": 30000,
+        "marketValueSum": 40000,
+        "totalSum": 50000,
+        "bot" : [
+            {
+                "botId": 1,
+                "botName" : "bot1",
+                "purchaseAmount": 60000,
+                "netProfit": -800,
+                "marketValue": 80000,
+                "total": 90000,
+                "strategy": 'bollinger',
+                "upperMovingAverage": 50,
+                "lowerMovingAverage": 20,
+                "upperK": 0.3,
+                "lowerK": 0.5,
+                "riskRate": 0.02,
+                "timeFrame": 'DAY',
+                "coinPair": 'KRW_BTC',
+                "orderHistory":[{
+                    "category":"BUY",
+                    "market":'KRW_BTC',
+                    "amount": 30000,
+                    "tradeDate": '2017-01-27',
+                    "price": 40000,
+                },{
+                    "category":"SELL",
+                    "market":'KRW_BTC',
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                }]
+            },
+            {
+                "botId": 1,
+                "botName" : "bot1",
+                "purchaseAmount": 60000,
+                "netProfit": -800,
+                "marketValue": 80000,
+                "total": 90000,
+                "strategy": 'bollinger',
+                "upperMovingAverage": 50,
+                "lowerMovingAverage": 20,
+                "upperK": 0.3,
+                "lowerK": 0.5,
+                "riskRate": 0.02,
+                "timeFrame": 'DAY',
+                "coinPair": 'KRW_BTC',
+                "orderHistory":[{
+                    "category":"BUY",
+                    "market":20000,
+                    "amount": 30000,
+                    "tradeDate": '2017-01-27',
+                    "price": 40000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                }]
+            },
+            {
+                "botId": 1,
+                "botName" : "bot1",
+                "purchaseAmount": 60000,
+                "netProfit": -800,
+                "marketValue": 80000,
+                "total": 90000,
+                "strategy": 'bollinger',
+                "upperMovingAverage": 50,
+                "lowerMovingAverage": 20,
+                "upperK": 0.3,
+                "lowerK": 0.5,
+                "riskRate": 0.02,
+                "timeFrame": 'DAY',
+                "coinPair": 'KRW_BTC',
+                "orderHistory":[{
+                    "category":"BUY",
+                    "market":20000,
+                    "amount": 30000,
+                    "tradeDate": '2017-01-27',
+                    "price": 40000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                },{
+                    "category":"SELL",
+                    "market":2000,
+                    "amount": 3000,
+                    "tradeDate": '2027-02-30',
+                    "price": 4000,
+                }]
+            }
+        ],
+    });
 
-        },
-    ]);
+    const ContextValue = useContext(Context);
+    const loginState = ContextValue.loginState;
+    const setLoginState = ContextValue.setLoginState;
+
+    useEffect(() => {
+        if(loginState){
+            axios.get('https://api.kobot.kro.kr/api/bot', {
+                headers: {
+                    Authorization:'Bearer ' + localStorage.getItem('loginToken')
+                }
+            }).catch(function (err){
+                if(err.response.data.message === 'notValidToken'){
+                    setLoginState(false)
+                    localStorage.removeItem('loginToken')
+                    alert('로그인이 만료되었습니다. 다시 로그인해주세요.')
+                }
+                else{
+                    if(err.response.data.message === '401 Unauthorized: \"{\"error\":{\"message\":\"This access key is incorrect.\",\"name\":\"invalid_access_key\"}}\"'){
+                        alert('유효하지 않은 키 값입니다.')
+                    }
+                    else{
+                        alert(err.response.data.message);
+                    }
+                }
+            }).then(function (response){
+                setBalance(response.data)
+            })
+        }
+    }, [])
 
     return(
         <div>
+            {loginState &&
             <MainCard>
                 <Grid container spacing={gridSpacing}>
-                    <Grid item xs={12} sm={3} align={'center'}>
+                    <Grid item xs={12} sm={2.4} align={'center'}>
                         <Grid>
                             <MuiTypography variant="h5" gutterBottom>
-                                Total Net Copy Amount
+                                총자산
                             </MuiTypography>
                         </Grid>
                         <Grid>
                             <MuiTypography variant="h2">
-                                ￦ {totalNetCopyAmount}
+                                ￦ {balance.totalSum}
                             </MuiTypography>
                         </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={3} align={'center'}>
+                    <Grid item xs={12} sm={2.4} align={'center'}>
                         <Grid>
                             <MuiTypography variant="h5" gutterBottom>
-                                Total Portfolio Margin Balance
+                                예수금
                             </MuiTypography>
                         </Grid>
                         <Grid>
                             <MuiTypography variant="h2">
-                                ￦ {totalPortfolioMarginBalance}
+                                ￦ {balance.moneyOnHand}
                             </MuiTypography>
                         </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={3} align={'center'}>
+                    <Grid item xs={12} sm={2.4} align={'center'}>
                         <Grid>
                             <MuiTypography variant="h5" gutterBottom>
-                                Total Realized PNL
+                                봇 투자금
                             </MuiTypography>
                         </Grid>
                         <Grid>
                             <MuiTypography variant="h2">
-                                ￦ {totalRealizedPNL}
+                                ￦ {balance.purchaseAmountSum}
                             </MuiTypography>
                         </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={3} align={'center'}>
+                    <Grid item xs={12} sm={2.4} align={'center'}>
                         <Grid>
                             <MuiTypography variant="h5" gutterBottom>
-                                Net Profit
+                                미실현수익
                             </MuiTypography>
                         </Grid>
                         <Grid>
                             <MuiTypography variant="h2">
-                                ￦ {netProfit}
+                                ￦ {balance.marketValueSum}
+                            </MuiTypography>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} sm={2.4} align={'center'}>
+                        <Grid>
+                            <MuiTypography variant="h5" gutterBottom>
+                                순이익
+                            </MuiTypography>
+                        </Grid>
+                        <Grid>
+                            <MuiTypography variant="h2">
+                                ￦ {balance.netProfitSum}
                             </MuiTypography>
                         </Grid>
                     </Grid>
                 </Grid>
             </MainCard>
-            <br/>
-            <MainCard title="Bot List" secondary={<AddBotModal/>}>
+            }
+            {!loginState &&
+            <MainCard>
                 <Grid container spacing={gridSpacing}>
-                    <Grid item xs={12} sm={12}>
-                        <SubCard title={botname} secondary={<MuiTypography>sdfsdf</MuiTypography>}>
-                            <Grid container>
-
-                            </Grid>
-                        </SubCard>
+                    <Grid item xs={12} sm={2.4} align={'center'}>
+                        <Grid>
+                            <MuiTypography variant="h5" gutterBottom>
+                                총자산
+                            </MuiTypography>
+                        </Grid>
+                        <Grid>
+                            <MuiTypography variant="h2">
+                                0
+                            </MuiTypography>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} sm={2.4} align={'center'}>
+                        <Grid>
+                            <MuiTypography variant="h5" gutterBottom>
+                                예수금
+                            </MuiTypography>
+                        </Grid>
+                        <Grid>
+                            <MuiTypography variant="h2">
+                                0
+                            </MuiTypography>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} sm={2.4} align={'center'}>
+                        <Grid>
+                            <MuiTypography variant="h5" gutterBottom>
+                                봇 투자금
+                            </MuiTypography>
+                        </Grid>
+                        <Grid>
+                            <MuiTypography variant="h2">
+                                0
+                            </MuiTypography>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} sm={2.4} align={'center'}>
+                        <Grid>
+                            <MuiTypography variant="h5" gutterBottom>
+                                미실현수익
+                            </MuiTypography>
+                        </Grid>
+                        <Grid>
+                            <MuiTypography variant="h2">
+                                0
+                            </MuiTypography>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} sm={2.4} align={'center'}>
+                        <Grid>
+                            <MuiTypography variant="h5" gutterBottom>
+                                순이익
+                            </MuiTypography>
+                        </Grid>
+                        <Grid>
+                            <MuiTypography variant="h2">
+                                0
+                            </MuiTypography>
+                        </Grid>
                     </Grid>
                 </Grid>
             </MainCard>
+            }
+            <br/>
+            {loginState &&
+            <MainCard title="봇 리스트" secondary={<AddBotModal/>}>
+                <Grid container spacing={gridSpacing}>
+                    {balance &&
+                    balance.bot.map((botInfo, index) => {
+                            return(<BotTemplate bot={botInfo} key={index}/>)
+                        }
+                    )}
+                </Grid>
+            </MainCard>
+            }
+            {!loginState &&
+            <MainCard title="자동매매 서비스">
+                <Typography textAlign={'center'} variant={'h2'}>
+                    로그인 후 이용가능합니다.
+                </Typography>
+            </MainCard>
+            }
 
         </div>
     )
